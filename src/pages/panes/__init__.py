91d8233410f351d9b8ece7f0d21df734528f4e2b
bcoombe <ben.coombe@transformuk.com>
2026-01-08 11:41:12 +0000

    Fix import errors

--- src/pages/panes/__init__.py
+++ src/pages/panes/__init__.py
@@ -2,7 +2,3 @@
 The Exposure Results contains 4 sub-tabs for different exposure analyses.
 Each has its own layout defined in separate files.
 """
-import exposure_results
-import exposure_summary
-import graph_average_exposure
-import graph_p975_exposure
